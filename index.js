db52107e702f2584e69c9e655b832a9f6d0d79fe
DezmonDND <denedoseikin@mail.ru>
2023-04-03 17:49:51 +0700

    feat: сделать кнопки JS

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const editBtn = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeBtn = document.querySelector('.popup__close-btn');
-
+const saveBtn = document.querySelector('.popup__save-btn');
+let popupContent = document.querySelector('.popup__content');
+let inputName = popupContent.querySelector('.popup__name');
+let inputDescription = popupContent.querySelector('.popup__description');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
 
 function openPopup() {
     popup.classList.add('popup__opened');
+    inputName.value = profileName.textContent;
+    inputDescription.value = profileDescription.textContent;
 }
 
 function closePopup() {
@@ -13,3 +20,15 @@ function closePopup() {
 
 editBtn.addEventListener('click', openPopup);
 closeBtn.addEventListener('click', closePopup);
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileDescription.textContent = inputDescription.value;
+    saveBtn.addEventListener('click', closePopup);
+}
+
+popupContent.addEventListener('submit', handleFormSubmit);
+
+
+
