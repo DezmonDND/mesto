efb42a2bf93d0c1cd3f2b34b78f3edbed5a17f3f
unknown <denedoseikin@mail.ru>
2023-04-22 09:34:10 +0700

    fix: исправить код после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -20,32 +20,6 @@ const popupPicture = document.querySelector('.popup__picture');
 const cardsTemplate = document.getElementById('cards-template');
 const elementsContainer = document.querySelector('.elements');
 
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
 // Popups
 function openPopup(modalWindow) {
     modalWindow.classList.add('popup_opened');
